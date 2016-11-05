34c328b2e4d9dd1e351b6bdc536afff2ce6b3c97
gladiator123 <medhsupcom@gmail.com>
2016-11-05 14:19:57 +0100

    backend working!!

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 
-app.use(express.static(_dirname+'/public'));
+app.use(express.static(__dirname+'/public'));
 
 app.get("/", function(req, res){
     res.render("first");
