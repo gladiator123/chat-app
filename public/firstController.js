290bd77583eec9864ff749d672ed71819af8926a
gladiator123 <medhsupcom@gmail.com>
2016-11-08 00:21:39 +0100

    moving to a frontend with angular

--- public/firstController.js
+++ public/firstController.js
@@ -1,6 +1,18 @@
 angular.module('MyApp',['ngMaterial'])
 
 .controller('AppCtrl', function($scope) {
+	$scope.messages = [];
+	$scope.name="AMINE";
+	var socket = io.connect('http://localhost:3700');
+	socket.on('message', function (data) {
+        if(data.message) {
+            $scope.messages.push(data);
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
 
-
+    $scope.send=function(){
+    	socket.emit('send', { message: $scope.message, username: $scope.name });
+    }
 });
